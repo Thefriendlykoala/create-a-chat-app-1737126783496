e3309fb2698aee4a73dffff3b2bb5e440c413c31
Thefriendlykoala <91312002+Thefriendlykoala@users.noreply.github.com>
2025-01-18 02:13:06 +1100

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "daisyui": {
+    "base": true,
+    "logs": true,
+    "utils": true,
+    "prefix": "",
+    "styled": true,
+    "themes": [
+      "light",
+      "dark",
+      "cyberpunk"
+    ],
+    "darkTheme": "dark",
+    "themeRoot": ":root"
+  },
+  "plugins": [
+    "daisyui"
+  ]
+}
